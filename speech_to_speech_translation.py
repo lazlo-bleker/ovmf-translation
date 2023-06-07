0fdce48c4c68c1d102a63e0f665e6f0cd0d6abe5
Lazlo Bleker <lazlo@Lazlos-Mac-mini.local>
2023-06-07 20:03:14 +0200

    updates

--- speech_to_speech_translation.py
+++ speech_to_speech_translation.py
@@ -3,6 +3,7 @@ import azure.cognitiveservices.speech as speechsdk
 API_KEY = '205881039a6c416783b9bb6397ca6f0e'
 ENDPOINT = 'https://westeurope.api.cognitive.microsoft.com/sts/v1.0/issuetoken'
 
+
 def translate_speech():
     # Creates an instance of a speech translation config with specified subscription key and endpoint.
     translation_config = speechsdk.translation.SpeechTranslationConfig(
@@ -14,6 +15,7 @@ def translate_speech():
 
     # Translate text to speech
     speech_config = speechsdk.SpeechConfig(subscription=API_KEY, endpoint=ENDPOINT)
+    speech_config.speech_synthesis_voice_name = "en-US-JessaNeural"
     speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config)
 
     # Creates a translation recognizer using and audio configuration
@@ -34,11 +36,26 @@ def translate_speech():
             print('Recognized: {}'.format(evt.result.text))
             print('Translated into English: {}'.format(evt.result.translations['en']))
 
-    def recognizing_cb(evt):
-        """callback that handles the `recognizing` event"""
-        print('Recognizing: {}'.format(evt.result.text))
+            # Speak the translated text with faster speed using SSML
+            ssml_text = """
+            <speak version='1.0' xmlns='https://www.w3.org/2001/10/synthesis' xmlns:mstts='https://www.w3.org/2001/mstts' xml:lang='en-US'>
+            <voice name='en-US-TonyNeural'>
+            <mstts:express-as style='default'>
+            {}
+            </mstts:express-as>
+            </voice>
+            </speak>
+            """.format(evt.result.translations['en'])
+            result_speech_synthesis = speech_synthesizer.speak_ssml_async(ssml_text).get()
 
-    recognizer.recognizing.connect(recognizing_cb)  # <<< NEW LINE HERE
+            if result_speech_synthesis.reason == speechsdk.ResultReason.SynthesizingAudioCompleted:
+                print('Speech synthesized for text [{}]'.format(result_speech_synthesis.text))
+            elif result_speech_synthesis.reason == speechsdk.ResultReason.Canceled:
+                cancellation_details = result_speech_synthesis.cancellation_details
+                print('Speech synthesis canceled: {}'.format(cancellation_details.reason))
+                if cancellation_details.reason == speechsdk.CancellationReason.Error:
+                    print('Error details: {}'.format(cancellation_details.error_details))
+
     recognizer.recognized.connect(recognized_cb)
     recognizer.session_stopped.connect(stop_cb)
 
@@ -51,5 +68,6 @@ def translate_speech():
 
     # Stop continuous recognition
     recognizer.stop_continuous_recognition()
+
 
 translate_speech()
