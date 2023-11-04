6cd285843d6c70a110abff69273e065fb1977f4c
Lazlo Bleker <lazlo@Lazlos-Mac-mini.local>
2023-11-04 12:37:53 +0100

    Added delay monitoring

--- speech_translator.py
+++ speech_translator.py
@@ -1,7 +1,13 @@
 import azure.cognitiveservices.speech as speechsdk
+import time
+import json
+import os
+import datetime
 
 
 class SpeechTranslator:
+    base_synth_time = 1.35712551
+    per_char_synth_time = 0.05745246
     """Class for live speech to speech translation."""
     def __init__(self, api_key: str, region: str = 'westeurope', source_language: str = 'de-DE',
                  target_language: str = 'en', intermediate_translations: bool = True, verbose: bool = True):
@@ -15,6 +21,17 @@ class SpeechTranslator:
         self.last_synthesized_index = 0  # To keep track of the last index of the last synthesized text
         self.intermediate_buffer = ["", "", ""]  # Buffer for storing the 3 most recent intermediate translations
         self.synthesized_text = ""  # Stores cumulative intermediate translations
+        self.speech_queue = []
+        self.last_synth_time = float('nan') # Time of last speech synthesis completion
+        self.last_recognized_time = time.time() # Time of last intermediate or final speech recognition
+
+        self.pr = 0
+        if os.path.exists(f'speech_data_pr{self.pr}.json'):
+            with open(f'speech_data_pr{self.pr}.json', 'r') as f:
+                self.speech_data = json.load(f)
+        else:
+            self.speech_data = {'prosody rate': self.pr, 'num_chars': [], 'time_per_char': []}
+
 
     def translate_speech(self):
         """Main method to initiate live speech translation. Audio in is taken from the operating system audio input."""
@@ -32,6 +49,8 @@ class SpeechTranslator:
         recognizer.recognized.connect(lambda evt: self._recognized_cb(evt, speech_synthesizer))
         if self.intermediate_translations:
             recognizer.recognizing.connect(lambda evt: self._recognizing_cb(evt, speech_synthesizer))
+        speech_synthesizer.synthesis_completed.connect(lambda evt: self._synthesis_completed_cb(evt))
+        speech_synthesizer.synthesis_started.connect(lambda evt: self._synthesis_started_cb(evt))
 
         # Start translation until interrupted
         recognizer.start_continuous_recognition()
@@ -39,6 +58,8 @@ class SpeechTranslator:
             while not self.done:
                 pass
         except KeyboardInterrupt:
+            with open(f'speech_data_pr{self.pr}.json', 'w') as f:
+                json.dump(self.speech_data, f)
             recognizer.stop_continuous_recognition()
 
     def _stop_cb(self, evt):
@@ -61,10 +82,12 @@ class SpeechTranslator:
             text_to_synthesize = evt.result.translations[self.target_language][self.last_synthesized_index:]
             self.last_synthesized_index = 0  # Reset the last synthesized text index
             self.synthesized_text = ""  # Reset cumulative intermediate translations
+            self.last_recognized_time = time.time()
             self._synthesize_text(text_to_synthesize, speech_synthesizer)
 
     def _recognizing_cb(self, evt, speech_synthesizer):
         """Callback method for when speech is being recognized and translated."""
+        self._log(f'Delay: {str(datetime.timedelta(seconds=int(self._speech_queue_time() + 0.5)))}')
         if evt.result.reason == speechsdk.ResultReason.TranslatingSpeech:
             # Update the intermediate translation buffer with the latest translation
             self.intermediate_buffer.pop(0)
@@ -83,7 +106,17 @@ class SpeechTranslator:
                     if len(text_to_synthesize) > 0:
                         self._log(f'Intermediate translation: {text_to_synthesize}')
                         self.synthesized_text += text_to_synthesize  # Update the until now synthesized text
+                        self.last_recognized_time = time.time()
                         self._synthesize_text(text_to_synthesize, speech_synthesizer)
+
+    def _synthesis_started_cb(self, evt):
+        self.last_synth_time = time.time()
+
+    def _synthesis_completed_cb(self, evt):
+        time_to_complete = time.time() - self.last_synth_time
+        self.speech_data['num_chars'].append(self.speech_queue[0])
+        self.speech_data['time_per_char'].append(time_to_complete/self.speech_queue[0])
+        self.speech_queue.pop(0)
 
     def _synthesize_text(self, text, speech_synthesizer):
         """Method to synthesize text into speech"""
@@ -91,14 +124,23 @@ class SpeechTranslator:
             "<speak version='1.0' xmlns='https://www.w3.org/2001/10/synthesis' "
             "xmlns:mstts='https://www.w3.org/2001/mstts' xml:lang='en-US'>"
             "<voice name='en-US-JennyNeural'>"
-            "<prosody rate='+0%'>"
+            f"<prosody rate='+{self.pr}%'>"
             "<mstts:express-as style='default'>"
             f"{text}"
             "</mstts:express-as>"
             "</prosody>"
             "</voice>"
             "</speak>")
+        self.speech_queue.append(len(text))
+        self._log(f'speech_queue = {self.speech_queue}')
         speech_synthesizer.speak_ssml_async(ssml_text)
+
+    def _speech_queue_time(self):
+        speech_queue_times = [self.per_char_synth_time*item + self.base_synth_time for item in self.speech_queue]
+        current_speech_time = time.time() - self.last_recognized_time
+        current_synth_time = time.time() - self.last_synth_time if self.speech_queue != [] else 0
+        speech_queue_time = sum(speech_queue_times) + current_speech_time - current_synth_time
+        return speech_queue_time
 
     def _log(self, message):
         if self.verbose:
